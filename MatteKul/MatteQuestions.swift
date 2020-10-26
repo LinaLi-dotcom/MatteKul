7f8433ed84644bae8905d4c9b32c6c89b1f16ecc
lina <linasigridli@gmail.com>
2020-10-26 21:39:29 +0100

    Funkar med en fråga med ett svar

---
+++ MatteKul/MatteQuestions.swift
@@ -0,0 +1,12 @@
+//
+//  MatteQuestions.swift
+//  MatteKul
+//
+//  Created by Lina Li on 2020-10-26.
+//
+
+import Foundation
+
+var MatteQuestions = [" 1 * 2 = 2"]
+
+
--- MatteKul/ViewController.swift
+++ MatteKul/ViewController.swift
@@ -8,12 +8,34 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var theProgressView: UIProgressView!
+    @IBOutlet weak var theTextView: UITextView!
+    @IBOutlet weak var theTrueButton: UIButton!
+    @IBOutlet weak var theFalseButton: UIButton!
+    @IBOutlet weak var theTextField: UITextField!
+    
+    
+    var mattequestion = ["1 * 2 = 2", "True"]
+   
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        theTextView.text = mattequestion[0]
     }
 
-
+    @IBAction func theAnswerButton(_ sender: UIButton) {
+        let theUserAnswer = sender.currentTitle!
+//  sender.currentTitle hämtar users answer
+        let theActualAnswer = mattequestion [1]
+        if  theUserAnswer == theActualAnswer{
+            theTextField.text = "Yeah! You are right!"
+        } else{
+            theTextField.text = "No...You are Wrong...Sorry"
+    }
+    
 }
 
+}
